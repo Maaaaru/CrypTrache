24839d3d6c742eb911cef94a15b552dfb16bd275
Maaaaru <50836297+Maaaaru@users.noreply.github.com>
2019-08-03 20:17:03 +0900

    Create GeneScienceInterface.sol

---
+++ GeneScienceInterface.sol
@@ -0,0 +1,5 @@
+contract MixGeneInterface {
+    function isGeneScience() public pure returns (bool);
+
+    function mixGenes(uint256 genes1, uint256 genes2, uint256 targetBlock) public returns (uint256);
+}
