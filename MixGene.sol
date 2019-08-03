3f497cf5a09f2e0553486e5669e8ffdf829463fc
Maaaaru <50836297+Maaaaru@users.noreply.github.com>
2019-08-03 20:21:38 +0900

    Update and rename GeneScienceInterface.sol to MixGene.sol

--- MixGene.sol
+++ MixGene.sol
@@ -1,4 +1,4 @@
-contract MixGeneInterface {
+contract MixGene {
     function isGeneScience() public pure returns (bool);
 
     function mixGenes(uint256 genes1, uint256 genes2, uint256 targetBlock) public returns (uint256);
