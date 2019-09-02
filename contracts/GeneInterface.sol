b0de3b80c594ccc120859cc822b06f3f55136e47
Maaaaru <kou0407hei@gmail.com>
2019-09-02 16:43:59 +0900

    GeneInterface-contracts

---
+++ contracts/GeneInterface.sol
@@ -0,0 +1,5 @@
+contract GeneInterface {
+    function isGeneScience() public pure returns (bool);
+
+    function geneInterface(uint256 genes1, uint256 genes2, uint256 targetBlock) public returns (uint256);
+}
